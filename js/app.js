53e69b917a9fd3e497b48a3745ef843086ed726e
Abdellatif Laghjaj <abdellatif.laghjaj@edu.uiz.ac.ma>
2022-08-06 14:35:28 +0100

    function to play audio

--- js/app.js
+++ js/app.js
@@ -21,16 +21,17 @@ createApp({
             fetch(full_url)
                 .then(response => response.json())
                 .then(data => {
-                    this.output = JSON.parse(JSON.stringify(data));
                     
                     //create the fields object
-                    this.fields.word = this.output[0].word;
-                    this.fields.phonetic = this.output[0].phonetic;
-                    this.fields.definition = this.output[0].meanings[0].definitions[0].definition;
-                    this.fields.example = this.output[0].meanings[0].definitions[0].example;
-                    this.fields.audio = this.output[0].phonetics[0].audio;
+                    this.fields.word = data[0].word;
+                    this.fields.phonetic = data[0].phonetic;
+                    this.fields.definition = data[0].meanings[0].definitions[0].definition;
+                    this.fields.example = data[0].meanings[0].definitions[0].example;
+                    this.fields.audio = data[0].phonetics[0].audio;
 
                     console.log(this.fields);
+
+                    this.output.push(this.fields);
 
                     // console.log("Original data: ", data);
                     // console.log("Output: ", this.output);
@@ -38,5 +39,9 @@ createApp({
                 })
                 .catch(error => alert(error));
         },
+        playAudio(audio){
+            const audio = new Audio(audio);
+            audio.play();
+        }
     },
 }).mount('#app');
