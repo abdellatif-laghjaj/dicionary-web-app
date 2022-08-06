80222425bb84a85f1744cb6f1cde0045051c051c
Abdellatif Laghjaj <abdellatif.laghjaj@edu.uiz.ac.ma>
2022-08-06 12:55:52 +0100

    add style

--- js/app.js
+++ js/app.js
@@ -2,6 +2,20 @@ new Vue({
     el: '#app',
     vuetify: new Vuetify(),
     data: {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        url: 'https://api.dictionaryapi.dev/api/v2/entries/en/hello',
+    },
+    methods: {
+        fetchData: function() {
+            fetch(this.url)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    console.log(data[0].license.name);
+                });
+        }
+    },
+    created: function() {
+        this.fetchData();
     }
 })
