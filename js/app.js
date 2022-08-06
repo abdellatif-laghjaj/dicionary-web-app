9a2f1f4b8eb225495367b5d5c0f09438376325cf
Abdellatif Laghjaj <abdellatif.laghjaj@edu.uiz.ac.ma>
2022-08-06 13:41:07 +0100

    start fetching data

--- js/app.js
+++ js/app.js
@@ -1,22 +1,16 @@
-const { createApp } = Vue
+import {createApp} from 'vue';
 
 createApp({
     data() {
         return {
             message: 'Hello Vue!',
-            url: 'https://api.dictionaryapi.dev/api/v2/entries/en/',
+            url: ' https://api.dictionaryapi.dev/api/v2/entries/en/',
             word: '',
         }
     },
     methods: {
-        search: function () {
-            const full_url = this.url + this.word
-            fetch(full_url)
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data);
-                }
-                );
+        searchWord() {
+            
         },
     },
 }).mount('#app');
